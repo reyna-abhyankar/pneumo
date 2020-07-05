e39d5f6bdad073ba8509c0711780c49ae8906fdc
virenabhyankar <forvirenra@gmail.com>
2020-07-05 13:05:23 -0700

    Add Xcode Project

---
+++ Pneumo/PneumoTests/PneumoTests.swift
@@ -0,0 +1,34 @@
+//
+//  PneumoTests.swift
+//  PneumoTests
+//
+//  Created by Viren Abhyankar on 7/5/20.
+//  Copyright © 2020 Viren Abhyankar. All rights reserved.
+//
+
+import XCTest
+@testable import Pneumo
+
+class PneumoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
