83c5ba8ec2b6ed9e7fc69762781774bfe4babb9a
alicechien26 <alicechien@g.ucla.edu>
2020-07-08 01:33:26 -0700

    Create Onboarding File

---
+++ Pneumo/Pneumo/Onboarding.swift
@@ -0,0 +1,21 @@
+//
+//  Onboarding.swift
+//  Pneumo
+//
+//  Created by Alice Chien on 7/8/20.
+//  Copyright © 2020 Viren Abhyankar. All rights reserved.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Onboarding_Previews: PreviewProvider {
+    static var previews: some View {
+        Onboarding()
+    }
+}
