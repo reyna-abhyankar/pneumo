33b730bf845de77734d400a15630d65020962cb2
virenabhyankar <forvirenra@gmail.com>
2020-08-22 23:01:35 -0700

    Fix continue button navigation

--- Pneumo/Pneumo/Onboarding.swift
+++ Pneumo/Pneumo/Onboarding.swift
@@ -19,6 +19,7 @@ struct WelcomeButtons: Identifiable {
 struct Onboarding: View {
     
     @State private var step = 1
+    @EnvironmentObject var viewRouter: ViewRouter
     
     let onboardingButtons = [WelcomeButtons(imageName: "P1",
                                             text: "With our powerful mobile app, pneumonia radiology analysis is made simple.",
@@ -36,72 +37,82 @@ struct Onboarding: View {
     let CONSTANT_OFFSET: CGFloat = 8
     
     var body: some View {
-            ZStack {
-                Color("DarkShade")
-                    .edgesIgnoringSafeArea(.all)
-                
-                VStack(spacing: 0) {
-                    Text("Welcome to")
-                        .font(.largeTitle)
-                        .fontWeight(.thin)
-                        .foregroundColor(Color("LightShade"))
-                        .padding(.top)
-                    Text("PNEUMO")
-                        .font(.largeTitle)
-                        .fontWeight(.regular)
-                        .bold()
-                        .foregroundColor(Color("LightShade"))
-                    
-                    GeometryReader { gr in
-                        HStack {
-                            ForEach(self.onboardingButtons, id: \.id) { onB in
-                                VStack(spacing: 10) {
-                                    Image(onB.imageName).resizable().frame(width: onB.imgWidth, height: onB.imgHeight)
-                                    Text("\(onB.text)")
-                                        .fontWeight(.thin)
-                                        .padding()
-                                        .fixedSize(horizontal: false, vertical: true)
-                                        .animation(Animation.interpolatingSpring(stiffness: 40, damping: 7).delay(0.1))
-                                }.frame(width: gr.frame(in: .global).width)
-                            }
-                        }.offset(y: -20)
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(Color("LightShade"))
-                        .font(.title)
-                        .padding(.vertical, 60).frame(width: gr.frame(in: .global).width * 3)
-                        .frame(maxHeight: .infinity).offset(x: self.step == 1 ? gr.frame(in: .global).width+self.CONSTANT_OFFSET : self.step == 2 ? 0 : -gr.frame(in: .global).width-self.CONSTANT_OFFSET)
-                        .animation(Animation.interpolatingSpring(stiffness: 40, damping: 8))
-                    }
-                    HStack(spacing: 20) {
-                        ForEach (1 ..< 4) { num in
-                            Button(action: {self.step = num}) {
-                                Image(systemName: "\(num).circle")
-                                    .padding()
-                                    .scaleEffect(self.step == num ? 1 : 0.65)
-                            }
-                        }
-                    }.offset(y: -40)
-                    .animation(.spring(response: 0.4, dampingFraction: 0.5))
+        ZStack {
+            Color("DarkShade")
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 0) {
+                Text("Welcome to")
                     .font(.largeTitle)
-                    .accentColor(Color("LightAccent"))
-                    
-                    NavigationLink(destination: Navigation()
-                        .navigationBarHidden(true)
-                        .navigationBarTitle("")) {
-                        HStack {
-                            Text("Continue")
-                            Image(systemName: "chevron.right")
+                    .fontWeight(.thin)
+                    .foregroundColor(Color("LightShade"))
+                    .padding(.top)
+                Text("PNEUMO")
+                    .font(.largeTitle)
+                    .fontWeight(.regular)
+                    .bold()
+                    .foregroundColor(Color("LightShade"))
+                
+                GeometryReader { gr in
+                    HStack {
+                        ForEach(self.onboardingButtons, id: \.id) { onB in
+                            VStack(spacing: 10) {
+                                Image(onB.imageName).resizable().frame(width: onB.imgWidth, height: onB.imgHeight)
+                                Text("\(onB.text)")
+                                    .fontWeight(.thin)
+                                    .padding()
+                                    .fixedSize(horizontal: false, vertical: true)
+                                    .animation(Animation.interpolatingSpring(stiffness: 40, damping: 7).delay(0.1))
+                            }.frame(width: gr.frame(in: .global).width)
                         }
-                        .padding(.horizontal)
-                        .padding()
-                        .background(Capsule()
-                        .fill(Color("Accent2")))
-                        .accentColor(Color("LightAccent"))
-                        .opacity(step == 3 ? 1 : 0)
-                        .animation(.none).scaleEffect(step == 3 ? 1 : 0.01).animation(Animation.interpolatingSpring(stiffness: 50, damping: 10, initialVelocity: 10))
+                    }.offset(y: -20)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color("LightShade"))
+                    .font(.title)
+                    .padding(.vertical, 60).frame(width: gr.frame(in: .global).width * 3)
+                    .frame(maxHeight: .infinity).offset(x: self.step == 1 ? gr.frame(in: .global).width+self.CONSTANT_OFFSET : self.step == 2 ? 0 : -gr.frame(in: .global).width-self.CONSTANT_OFFSET)
+                    .animation(Animation.interpolatingSpring(stiffness: 40, damping: 8))
+                }
+                HStack(spacing: 20) {
+                    ForEach (1 ..< 4) { num in
+                        Button(action: {self.step = num}) {
+                            Image(systemName: "\(num).circle")
+                                .padding()
+                                .scaleEffect(self.step == num ? 1 : 0.65)
+                        }
+                    }
+                }.offset(y: -40)
+                .animation(.spring(response: 0.4, dampingFraction: 0.5))
+                .font(.largeTitle)
+                .accentColor(Color("LightAccent"))
+                
+                if step == 3 {
+                    Button(action: {
+                        withAnimation {
+                            self.viewRouter.currentPage = "Navigation"
+                        }
+                    }) {
+                        ContinueButton()
                     }
                 }
             }
+        }
+    }
+}
+
+struct ContinueButton: View {
+    var body: some View {
+        HStack {
+            Text("Continue")
+            Image(systemName: "chevron.right")
+        }
+        .padding(.horizontal)
+        .padding()
+        .background(Capsule()
+        .fill(Color("Accent2")))
+        .accentColor(Color("LightAccent"))
+        .opacity(1)
+        .animation(.none).scaleEffect(1).animation(Animation.interpolatingSpring(stiffness: 50, damping: 10, initialVelocity: 10))
     }
 }
 
