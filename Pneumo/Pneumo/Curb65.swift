6c4e0ba4d4284c0450479fbedaead75071c9f257
alicechien26 <alicechien@g.ucla.edu>
2020-08-29 22:04:46 -0700

    CURB65 Changes

--- Pneumo/Pneumo/Curb65.swift
+++ Pneumo/Pneumo/Curb65.swift
@@ -19,28 +19,47 @@ struct Curb65: View {
     
     var body: some View {
         VStack {
+            Text("CURB-65 ASSESSMENT")
+                .font(.largeTitle)
+                .fontWeight(.light)
+                .foregroundColor(Color("DarkShade"))
+                .padding()
+                .offset(y: -60)
             VStack {
                 ForEach (0 ..< tests.count) { index in
                     VStack(alignment: .leading) {
                         Text("\(self.tests[index])").font(.system(size: 18, weight: .semibold)).lineLimit(2)
+                            Divider()
                         HStack {
                             if self.toggleArr[index] {
                                 Text("Yes")
+                                    .foregroundColor(Color("Accent2"))
                             } else {
                                 Text("No")
+                                    .foregroundColor(Color("Accent2"))
                             }
                             Spacer()
                             Toggle("", isOn: self.$toggleArr[index])
                         }
                     }
-                    .padding()
+                    .padding(.leading, 30)
+                    .padding(.trailing, 30)
+                    .padding(.top, 10)
                 }
-            }
+            }.offset(y: -50)
             .toggleStyle(MyToggleStyle())
             .padding(EdgeInsets(top: 15, leading: 10, bottom: 15, trailing: 10))
             .navigationBarTitle("Resources", displayMode: .inline)
             
-            Text("Number of risk factors: \(getNumTrue(arr: toggleArr))")
+            VStack {
+                Text("Number of risk factors: \(getNumTrue(arr: toggleArr))")
+                    .font(.title)
+                    .fontWeight(.light)
+                    .foregroundColor(Color.black)
+            }.padding(20)
+            .overlay(
+                RoundedRectangle(cornerRadius: 16)
+                    .stroke(Color("Accent2"), lineWidth: 2))
         }
     }
     
