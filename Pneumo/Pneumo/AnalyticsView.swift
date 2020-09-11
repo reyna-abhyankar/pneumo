9050f797ce61d259007f0de58d99a981c921f514
alicechien26 <alicechien@g.ucla.edu>
2020-09-11 12:46:46 -0700

    AnalyticsView

---
+++ Pneumo/Pneumo/AnalyticsView.swift
@@ -0,0 +1,76 @@
+//
+//  AnalyticsView.swift
+//  Pneumo
+//
+//  Created by Alice Chien on 8/22/20.
+//  Copyright © 2020 Alice Chien. All rights reserved.
+//
+
+import SwiftUI
+
+struct AnalyticsView: View {
+    var body: some View {
+        VStack(spacing: 80) {
+            VStack {
+                Image("P4")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 250)
+                    .border(Color.gray, width:5)
+                    .offset(y: 35)
+                Text("PNEUMONIA DETECTED")
+                    .font(.largeTitle)
+                    .fontWeight(.light)
+                    .foregroundColor(Color("DarkShade"))
+                    .multilineTextAlignment(.center)
+                    .offset(y: 40)
+            }
+            HStack(spacing: 8) {
+                Image("P5")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50)
+                    .padding()
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 100)
+                            .stroke(Color.black, lineWidth: 2)
+                )
+                VStack {
+                    Text("Covid-Induced")
+                        .font(.headline)
+                        .padding(5)
+                    Text("Prognosis Accuracy: 93%")
+                        .padding(5)
+                        .offset(x: 8)
+                }
+            }.offset(y: -10)
+            VStack {
+                Text("Additional Results:")
+                    .padding(5)
+                    .font(.headline)
+                Text("Community-Aquired: 9%")
+                    .padding(5)
+                Text("No Pneumonia: 3%")
+                    .padding(5)
+            }.offset(y: -20)
+            HStack {
+                Text("Treatment Plans")
+                    .foregroundColor(.white)
+                    .padding(20)
+                    .background(Color("Accent2"))
+                    .cornerRadius(100)
+                Text("Save to Patient")
+                    .foregroundColor(.white)
+                    .padding(20)
+                    .background(Color("Accent2"))
+                    .cornerRadius(100)
+            }.offset(y: -20)
+        }
+    }
+}
+
+struct AnalyticsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnalyticsView()
+    }
+}
